3ed4b79eaed1ed40b2ce7f1ac2185172b47e75ab
JAYA PRAKASH PEDIREDLA <45528469+Jayaprakashpediredla@users.noreply.github.com>
2020-02-12 11:28:42 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import textblob
 
-consumer_key='alh3JMDgGXDjtWWshSxU7l048'
-consumer_secret='FlCZQVjp7WuD6UfWhr6JlQGaOPqxh9uS56oWPfIM2Bk1EPPzff'
+consumer_key='XDsPC3y9JMQrT4t9xrJf8gp6o'
+consumer_secret='S3PD0a8F4DZg959k1D8kpQer0eWoFscs129ka4pCZxOqi7wQni'
 
-access_token='581756115-QPC9CgAIOTkN3LLCkulMnE0Swir0FB5RAo6Ad0gi'
-acces_token_secret='jvl3Ih0tmPDONgweAqDcs780FMeIgwJunpOyIu2I9j2xA'
+access_token='581756115-sljf6KFrUxXdd0VIdzWiULtfIkDBpjqkb4oKVj7u'
+acces_token_secret='dlG3pHAiYIHMe4Sl2skA0qbKClkFzUuQhVdm6mwZzcQRo'
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,acces_token_secret)
